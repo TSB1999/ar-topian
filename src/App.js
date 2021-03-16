1cdd0bae60b7795babb541767c23ada53a1ae617
Tobi Shitta-Bey <tsb99@Tobis-MacBook-Pro-2.local>
2021-03-16 16:00:47 +0000

    feat : building buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./styles/main.scss";
 import Navbar from './elements/organisms/navbar/'
+import Button from './elements/atoms/buttons'
 
 function App() {
   return <div className="App">
     <Navbar/>
+    <Button title = "REGISTER"/>
+    <Button title = "SIGN IN"/>
   </div>;
 }
 
---
+++ src/elements/atoms/buttons/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Button(props) {
+    return (
+        <button>
+            <h1>{props.title}</h1>
+        </button>
+    )
+}
