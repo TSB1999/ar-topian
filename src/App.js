a6cdbd854220b66a9f9d282d508ee0159a9a13e9
Tobi Shitta-Bey <tsb99@Tobis-MacBook-Pro-2.local>
2021-03-17 00:55:40 +0000

    feat : landing organism impl

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./styles/main.scss";
 import Navbar from './elements/organisms/navbar/'
+import Landing from './elements/organisms/landing/'
 import LandingButtons from './elements/molecules/landing-buttons'
 
 function App() {
   return <div className="App">
     <Navbar/>
-    <LandingButtons/>
+    {/* <LandingButtons/> */}
+    <Landing/>
 
   </div>;
 }
