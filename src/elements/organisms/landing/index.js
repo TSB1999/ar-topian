6559658299a5bbfa4dea3e684c9c4194be3b3a2b
Tobi Shitta-Bey <tsbmedia@Tobis-Mac-mini.local>
2021-03-17 10:33:19 +0000

    feat : landing organism in progress

--- src/elements/organisms/landing/index.js
+++ src/elements/organisms/landing/index.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import LandingButton from "../../molecules/landing-buttons";
 
 export default function Landing() {
-    return (
-        <div className = "landing">
-            
-        </div>
-    )
+  return (
+    <div className="landing">
+      <h1 className="landing__heading">Get your ARtopian merch now!</h1>
+      <h3 className="landing__subheading">BRING YOUR ITEMS TO LIFE</h3>
+      <div className="landing__buttons">
+        <LandingButton />
+      </div>
+    </div>
+  );
 }
