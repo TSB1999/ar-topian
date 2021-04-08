59a0c55df246e522563d12ee08ec8dd28a62493b
TSB M3DIA <tsbmedia@TSBs-Mac-mini.local>
2021-04-08 16:49:02 +0100

    commit to work in library

---
+++ src/3.containers/registrationFormView/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import RegistrationForm from "../../4.elements/2.organisms/registration";
+
+export default function index({ ...props }) {
+  return <RegistrationForm {...props} />;
+}
